617eb7402b5afa85ff0ad85930f3dd39c167c767
peterregal8 <peterregal8@gmail.com>
2025-03-01 12:50:35 -0800

    Created background.js to have link open on browser startup

---
+++ background.js
@@ -0,0 +1,13 @@
+const links = [
+    'https://mynews13.com/fl/orlando/news/2024/01/22/kissimmee-kid-considered-a-genius-at-age-5',
+    'https://www.kidschaupal.com/young_xpert/soborno-isaac-bari',
+    'https://www.braingainmag.com/meet-kairan-quazi-who-is-only-nine-and-already-in-college.htm',
+    'https://www.forbes.com/profile/livia-voigt/'
+]
+
+function openRandomLink() {
+    const randomLink = links[Math.floor(Math.random() * links.length)];
+    chrome.tabs.create({url: randomLink});
+}
+
+chrome.runtime.onInstalled.addListener(openRandomLink);
