36e2164777d513a147053248929cc856a583776d
peterregal8 <peterregal8@gmail.com>
2025-02-28 23:13:18 -0800

    Edited link to 7 year old genius

--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
 document.getElementById('openLink').addEventListener('click', function() {
-    window.open('https://example.com', '_blank');
+    window.open('https://mynews13.com/fl/orlando/news/2024/01/22/kissimmee-kid-considered-a-genius-at-age-5', '_blank');
 });
