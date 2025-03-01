446f9f62c5bf751444698abba74cdd8be24b252c
peterregal8 <peterregal8@gmail.com>
2025-02-28 23:45:12 -0800

    Added the ability to choose a random link

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
 document.getElementById('openLink').addEventListener('click', function() {
-    window.open('https://mynews13.com/fl/orlando/news/2024/01/22/kissimmee-kid-considered-a-genius-at-age-5', '_blank');
+
+    const links = [
+        'https://mynews13.com/fl/orlando/news/2024/01/22/kissimmee-kid-considered-a-genius-at-age-5',
+        'https://www.kidschaupal.com/young_xpert/soborno-isaac-bari',
+        'https://www.braingainmag.com/meet-kairan-quazi-who-is-only-nine-and-already-in-college.htm',
+        'https://www.forbes.com/profile/livia-voigt/'
+    ]
+
+    const randomLink = links[Math.floor(Math.random() * links.length)];
+
+    window.open(randomLink, '_blank');
 });
