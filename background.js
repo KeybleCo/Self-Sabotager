4ebf2abdc2520cc0c67ed3376ef08f8eaf8393b4
peterregal8 <peterregal8@gmail.com>
2025-03-01 17:23:31 -0800

    Updating first tab on startup

--- background.js
+++ background.js
@@ -13,7 +13,13 @@ function openRandomLink() {
 
 chrome.runtime.onInstalled.addListener(openRandomLink);
 
-chrome.runtime.onStartup.addListener(openRandomLink);
+chrome.runtime.onStartup.addListener(function(window) {
+    chrome.tabs.query({windowId: window.id}, function(tabs) {
+        if (tabs.length === 1) {
+            chrome.tabs.update(tabs[0].id, {url: randomLink});
+        }
+    });
+});
 
 chrome.windows.onCreated.addListener(function(window) {
     chrome.tabs.query({windowId: window.id}, function(tabs) {
