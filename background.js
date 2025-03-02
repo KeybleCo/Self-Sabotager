d01747cc4b3089efa2e0deef84769995e4f980f7
peterregal8 <peterregal8@gmail.com>
2025-03-01 18:12:54 -0800

    Added more links

--- background.js
+++ background.js
@@ -2,7 +2,9 @@ const links = [
     'https://mynews13.com/fl/orlando/news/2024/01/22/kissimmee-kid-considered-a-genius-at-age-5',
     'https://www.kidschaupal.com/young_xpert/soborno-isaac-bari',
     'https://www.braingainmag.com/meet-kairan-quazi-who-is-only-nine-and-already-in-college.htm',
-    'https://www.forbes.com/profile/livia-voigt/'
+    'https://www.forbes.com/profile/livia-voigt/',
+    'https://www.fortnite.com/item-shop?lang=en-US',
+    'https://x.com/canadahonk'
 ]
 
 const randomLink = links[Math.floor(Math.random() * links.length)];
